837759b3dfbfe56564269a42b083e9096f442275
LucasVoelz <Lucas.voelz@googlemail.com>
2016-12-09 21:38:19 +0100

    Vorerst fertig
    so ungefähr soll die Klasse am Ende aussehen, kleinere Veränderungen sollte es noch geben, aber nichts grundlegendes mehr

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 # -*- coding: cp1252 -*-
-#Datum :    07.12.16
+#Datum :    07-09.12.16
 #Autor/en:  Kerim, Till, Lucas V.
 #Version:   1.0
 
@@ -18,36 +18,38 @@ Wir machen:
 
 
 Wir brauchen:
-- Spielerposition und Blickrichtung
-- alle Spieler an/abmelden mit login/logoutPlayers
-- lebendeSpieler (an/abmelden in login/logoutAlivePlayers)
-- Aktive schuesse (Position, Vektor ueber addShot)
-- Map (Waende, damit der Schuess nur bis zur Wand fliegt)
-- Raueme (welcher Spieler in welchem Raum ist, am besten auch mit an/abmelden der Spieler)
+- PlayerObjekte mit
+    - nick, position, Blickrichtung, hp, speed und weapon
+    - Aktive schuesse (Position, Vektor ueber addShot)
+    - Map (Waende, damit der Schuess nur bis zur Wand fliegt)
 '''
 
 class Game():
     #Startet das Spiel, gibt Startpositionen durch
-    #achtet DURCHGEHEND wo die lebenden Spieler sind, und zeigt diese allen Spielern im selben Raum
-    #Bei einem Schuss, wird gemeldet ob und wer getroffen wurde, (bei einer toetung wird dies gemeldet und der Spieler rausgenommen {in der Klasse Spieler})
+    #achtet DURCHGEHEND wo die lebenden Spieler sind
+    #Bei einem Schuss, wird geprueft ob und wer getroffen wurde, (bei einer toetung wird er ausm Spiel genommen)
     #Beendet das Spiel, sobald nur noch ein Spieler lebt
     
     def __init__(self):
-        self.players = [] #liste mit IPs/Nicknames
-        self.alivePlayers = [] #liste mit IPs/Nicknames
+        self.players = [] #liste mit Objekten(Players)
         self.activeShots = [] #liste aus objekten(shots)
 
         while 1:
             self.updateAllPlayers()
             self.updateAllShots()
+            if len(players) == 0:
+                self.endGame()
      
     def updateAllPlayers(self):
         for player in self.alivePlayers:
             self.getPlayerMovement(player)
+            zeige das den clients z.T. kp wie #absichtlich kein Kommentar, damit das beim probieren sofort gefunden wird!
     
     def updateAllShots(self):
-        for shot in self.aktiveShots:
-            self.__calculateFlight(shot)
+        for bullet in self.aktiveShots:
+            bullet.move()
+            self.__handleCollision(bullet)
+            
             
     def getPlayerMovement(self, player):
         #returnt x,y Koordinate (Tuple) und Blickrichtung (Int, Int)
@@ -57,41 +59,46 @@ class Game():
 
     def getPlayerPosition(self, player):
         #returnt die x,y Koordinaten vom Player als Tuple (Int, Int)
-        #von wo bekommen wir die?
-    
+        return player.getPosition()
+        
     def getPlayerDirection(self, player):
         #returnt die Blickrichtung vom Player, als Tuple (Int, Int)
-        #von wo bekommen wir die?
+        return player.getDirection()
 
 
     def addShot(self, bullet):
         self.activeShots.append(bullet)
             
-    def __calculateFlight(self, bullet):
-        #hole die Positionen der Shots, und den Vektor
-        #bewege die Kugeln & pruefe fuer jede Kugel ob Wand, oder Spieler getroffen wird
-        #falls ja, nimm den shot raus, sonst Kugel weiterfliegen lassen
-        #wenn ein Spieler getroffen wird, dies auch dem Spieler
-        bullet.move()
-        bullet.getPosition()
-        if bullet.getPosition() in self.wallCoordinates:
+    def __handleCollision(self, bullet):
+        #hole die Positionen der Shots & prueft fuer jede Kugel ob sie in einer Wand, oder einem Spieler ist
+        #falls ja, nimm den shot raus beim Spiler wird hp abgezogen & ueberpruefung ob er Tod ist
+        bulletPos = bullet.getPosition()
+        if bulletPos in self.wallCoordinates:# <-- wallcoords muessen erst iwie geholt werden
             self.activeShots.remove(bullet)
-        elif bullet.getPosition() in self.playerCoordinates:
-            Player.hit(shot[3])#shot[3] = dmg
+        for i in range(len(self.players)):
+            if bulletPos == players[i].getPosition():# <-- playerumfang fehlt noch sonst muesste man einen
+                self.playerIsHit(players[i], bullet) # bestimmten Pixel treffen
+            self.playerIsHit()
             self.activeShots.remove(bullet)
+    
+    def playerIsHit(self, player, bullet):
+        newHp = player.getHitpoints() - bullet.getDamge()
+        player.setHitpoints(newHp)
+        if newHp <= 0:
+            self.players.remove(player)
 
+    def alivePlayers(self):
+        alivePlayers = []
+        for alivePlayer in self.players:
+            if aliveplayer.getHitpoints > 0:
+                alivePlayers.append(alivePlayer)
+        return alivePlayers
+    
     def addPlayer(self, player):
         self.players.append(player)
 
     def removePlayer(self, player):
         self.players.remove(player)
 
-    def addAlivePlayer(self, player):
-        self.alivePlayers.append(player)
-
-    def removeAlivePlayer(self, player):
-        self.alivePlayers.remove(player)
-
-    def updateAllShots(self):
-        for shot in self.activeShots:
-            self.__calculateFlight(shot)
+    def endGame(self):
+        print 'das Spiel ist zuende und gewonnen hat: ' + str(players[0]), + '!'
