de320c7d3820f2aaf919ee47d509515ceb091743
LucasVoelz <Lucas.voelz@googlemail.com>
2016-12-14 17:03:39 +0100

    Tests, Korrektur & Kommentation

--- bullet.py
+++ bullet.py
@@ -1,5 +1,5 @@
 # -*- coding: cp1252 -*-
-#Datum :    07.12.16
+#Datum :    07-14.12.16
 #Autor/en:  Lucas V.
 #Version:   1.0
 
@@ -10,39 +10,36 @@ import math
 Ein Bullet ist eine Kugel mit einer Position, Bewegungsrichtung, Geschwindigkeit und Schaden.
 Ein Bullet wird immer von einer Waffe erzeugt
 '''
+
+'''
+Todo:
+ -eine an und abmeldeautomatik fuer Sektoren (in arbeit)
+'''
  
 class Bullet(Entity):
-    def __init__(self, rect = ((0, 0), (2, 5)), direction = (0, 0), speed = 1, damage = 2):
-        self = super.__init__(rect, direction, speed)
+    def __init__(self, rect = (0, 0, 1, 2), direction = (1, 1), speed = 1, damage = 2):
+        Entity.__init__(self, rect, direction, speed)
         self.dmg = damage
-        self.__pixelLength = 10
- 
+        self.__pixelLength = 10 #Int
+        self.__r = self.__pixelLength / math.hypot(direction[0], direction[1]) #Int / (Int^2 + Int^2) 
+        #r * |direction| = pixelLength --> r = pixelLength / |direction| --> r * |direction| kann alle 10 pixel (self.__pixelLength) berechnet werden
+        ##|direction| = directionlaenge in Pixel        
+        
+        
     def move(self):
-        '''
-        Ziel: der Schuss soll nach 10p berechnet werden
-        Position = (x, y)
-        direction = (n, m)
-        |direction| = direction in Pixel
-        r * |direction| = pixelLength --> r = pixelLength / |direction| --> r * |direction| kann alle 10 pixel berechnet werden
-        DANACH kann man weiterrechnen
-        r * (direction) = neue Position
-        neueXPos += r * direction[0]
-        neueYPos += r * direction[1]
-        '''
-        x = self.position[0]
-        y = self.position[1]
-        directionX = self.direction[0]
-        directionY = self.direction[1]
-        r = self.__pixelLength / math.hypot(directionX, directionY)
+        #setzt die Kugel __r * speed pixel in Vektorrichtung weiter
+        x = self.rect[0] #Int
+        y = self.rect[1] #Int
+        directionX = self.direction[0] #Int
+        directionY = self.direction[1] #Int  
+        newXPosition = x + self.__r * directionX * self.speed #Int + float * Int * float
+        newYPosition = y + self.__r * directionY * self.speed #Int + float * Int * float
         
-        newXPosition = x + r * directionX
-        newYPosition = y + r * directionY
+        self.rect[0] = int(newXPosition)
+        self.rect[1] = int(newYPosition)
         
-        self.position[0] = int(newXPosition)
-        self.position[1] = int(newYPosition)
-        
-    def getDamge(self):
+    def getDamage(self):#Int
         return self.dmg
     
-#    def setDamge(self, dmg):
+#    def setDamage(self, dmg): #Int
 #        self.dmg = dmg
