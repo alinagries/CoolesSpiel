4acbeee658dd1c38817d4a67d7f8284eb622f7ef
LucasVoelz <Lucas.voelz@googlemail.com>
2016-12-14 17:04:46 +0100

    Tests, Korrektur & Kommentare

--- entity.py
+++ entity.py
@@ -1,5 +1,5 @@
 # -*- coding: cp1252 -*-
-#Datum :    09/10.12.16
+#Datum :    09-14.12.16
 #Autor/en:  Lucas V.
 #Version:   1.0
 
@@ -11,38 +11,43 @@ Player und Bullet erben von der Klasse
 
 
 class Entity():
-    def __init__(self, rect = ((0, 0), (0, 0)), direction = (0, 0), speed = 1):
-        self.rect = pygame.Rect(rect) #rect bekommt ((left, top), (width, height))# Koordianten und Umfang
-        self.position = rect[0]
+    def __init__(self, rect = (0, 0, 0, 0), direction = (0, 0), speed = 1):
+        self.rect = pygame.Rect(rect) #rect bekommt (left, top, width, height)# Koordianten und Umfang
         self.direction = direction
         self.speed = speed
 
-    def getRect(self):
+    def getSize(self):
+        return self.rect[2], self.rect[3] #(Int, Int)
+
+    def setSize(self, width, height): #(Int, Int)
+        self.rect = pygame.Rect(self.rect[0], self.rect[1], width, height)
+
+    def getRect(self): #(Int, Int, Int, Int)
         return self.rect
     
-    def setRect(self, (xCoord, yCoord), (width, height)):
-        self.rect = (xCoord, yCoord), (width, height)
+    def setRect(self, xCoord, yCoord, width, height): #(Int, Int, Int, Int)
+        self.rect = pygame.Rect(xCoord, yCoord, width, height)
  
-    def getPosition(self):
+    def getPosition(self): #(Int, Int)
+        return self.rect[0], self.rect[1]
+
+    def setPosition(self, x, y): #(Int, Int)
+        self.rect = pygame.Rect(x, y, self.rect[2], self.rect[3])
+    
+    def getXPos(self): #Int
         return self.rect[0]
     
-    def setPosition(self, x, y):
-        self.rect[0] = x, y
-    
-    def getXPos(self):
-        return self.rect[0][0]
-    
-    def getYPos(self):
-        return self.rect[0][1]
+    def getYPos(self): #Int
+        return self.rect[1]
 
-    def getDirection(self):
+    def getDirection(self): #(Int, Int)
         return self.direction
     
-#    def setDirection(self, x, y):
+#    def setDirection(self, x, y): #(Int, Int)
 #        self.direction = x, y
         
-    def getSpeed(self):
+    def getSpeed(self):#soll Float sein, bisher nur mit Ints benutzt
         return self.speed
     
-#    def setSpeed(self, speed):
+#    def setSpeed(self, speed): #soll Float sein, wird bisher aber nur mit Int benutzt
 #        self.speed = speed
