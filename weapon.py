700f8f32a630a195d89e22bb3f1b00d69e6fd957
LucasVoelz <Lucas.voelz@googlemail.com>
2016-12-14 17:08:53 +0100

    Tests, Korrektur & Kommentare

--- weapon.py
+++ weapon.py
@@ -1,9 +1,12 @@
 # -*- coding: cp1252 -*-
-#Datum :    07.12.16
-#Autor/en:  Lucas V.
+#Datum :    07-14.12.16
+#Autor/en:  Lucas V., Till
 #Version:   1.0
 
+from decimal import Decimal
 from bullet import Bullet
+import time
+
 '''
 Weapons besitzen
  -ein Richtung (die kann auch eine andere sein, als die vom player)
@@ -14,53 +17,67 @@ Weapons besitzen
  -und Munition
  '''
  
- #time muss mit einbezogen werden, damit die firerate irgendeinen Sinn macht
+'''
+Todo:
+ -Bei Langeweiele
+    -max reichweite fuer Waffen einbauen(?) fuer Streuschuesse, Bsp. Flammenwerfer, schrotgewehr oder so
+    -weitere Waffen (Streuschuesse) zu machen: z.B. sniper, maschinengewehr, panzerfaust? raketenwerfer? ganz nach geschmack
+    -andere gadets, z.B. minen, andere Fallen, oder sowas
+        -tarnzeugs (bsp. n paar sec nicht zu sehen, oder man muss naeher an der Person sein um sie zu sehen), Teleporter, etc....
+        -sichtfeld erhoehen
+        -Waffe upgraden? (feuerrate veringern, schaden erhoehen, ...)
+'''
+ 
 class Weapon():
-    def __init__(self, direction = (0, 0), firerate = 1, bulletspeed = 1, damge = 2, ammo = True): # True = unendlich, int = endlich
-        self.bulletsize = (2, 5)
+    def __init__(self, direction = (1, 1), firerate = 1, bulletspeed = 1, damage = 2, ammo = Decimal("Infinity")):
+        self.bulletsize = (1,3)
         self.direction = direction
         self.firerate = firerate
-        self.bulletspeed = bulletSpeed
-        self.dmg = damge
+        self.bulletspeed = bulletspeed
+        self.dmg = damage
         self.ammo = ammo
+        self.lastShotTime = time.clock()
 
-    def createBullet(self, position):
-        bullet = Bullet.__init__((position, self.bulletsize), self.direction, self.bulletSpeed, self.dmg)
-        return bullet
+
+    def createBullet(self, position): #(Int, Int)
+        if self.shotAllowed(): #Boolean
+            self.ammo -= 1
+            bullet = Bullet((position, self.bulletsize), self.direction, self.bulletspeed, self.dmg)
+            return bullet #Objekt
+        else:
+            print 'shot not Allowed'
     
-    
-    def getDirection(self):
+    def shotAllowed(self): #Boolean
+        if time.clock() - (self.lastShotTime + self.firerate) > 0 and self.ammo:
+            self.lastShotTime = time.clock()
+            return True
+            
+    def getDirection(self): #(Int, Int)
         return self.direction
     
-    def setDirection(self, x, y):
+    def setDirection(self, x, y): #(Int, Int)
         self.direction = x, y
-    
-    def getPosition(self):
-        return self.position
-    
-    def setPosition(self, x, y):
-        self.position = x, y
         
-    def getFirerate(self):
+    def getFirerate(self): #Float
         return self.firerate
     
-    def setDirection(self, firerate):
+    def setFirerate(self, firerate): #Float
         self.firerate = firerate
         
-    def getBulletspeed(self):
+    def getBulletspeed(self): #Float
         return self.bulletspeed
     
-    def setBulletspeed(self, bulletspeed):
+    def setBulletspeed(self, bulletspeed): #Float
         self.bulletspeed = bulletspeed
         
-    def getDamage(self):
+    def getDamage(self): #Float/Double
         return self.dmg
     
-    def setDamage(self, dmg):
+    def setDamage(self, dmg): #Float oder Double
         self.dmg = dmg
     
-    def getAmmo(self):
+    def getAmmo(self): #Int oder Decimal("Infinity")
         return self.ammo
     
-    def setAmmo(self, ammo):
+    def setAmmo(self, ammo): #Int oder Decimal("Infinity")
         self.ammo = ammo
