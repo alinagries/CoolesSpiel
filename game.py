203363541393c275c4b875fe828fcbea7f16a6a1
LucasVoelz <Lucas.voelz@googlemail.com>
2016-12-14 17:10:50 +0100

    Weiß ich auch nicht mehr so genau

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 # -*- coding: cp1252 -*-
-#Datum :    07-09.12.16
+#Datum :    07-14.12.16
 #Autor/en:  Kerim, Till, Lucas V.
 #Version:   1.0
 
@@ -24,6 +24,17 @@ Wir brauchen:
     - Map (Waende, damit der Schuess nur bis zur Wand fliegt)
 '''
 
+'''
+Todo:
+ -DC und reconnect einbauen (bsp. nach 10 sec dc wird sein hp auf 0 gesetzt und ist auch in der naechsten runde 0,
+    bis er sich reconnected, dann kann er ab einer neuen Runde weiterspielen)
+ -dauernde Kommunikation mitm Client (ich glaub Alina arbeitet grad dadran)
+ -Den Server, mit den IPs verwalten/einrichten oder wie auch immer das heisst (ich glaub Alina arbeitet dadran)
+ -generell ob die Kommentare stimmen
+ -Ovidiu fragen, was er noch alles hier haben will
+'''
+
+
 class Game():
     #Startet das Spiel, gibt Startpositionen durch
     #achtet DURCHGEHEND wo die lebenden Spieler sind
@@ -37,13 +48,15 @@ class Game():
         while 1:
             self.updateAllPlayers()
             self.updateAllShots()
-            if len(players) == 0:
+            if len(players) == 1:
                 self.endGame()
      
     def updateAllPlayers(self):
-        for player in self.alivePlayers:
-            self.getPlayerMovement(player)
-            zeige das den clients z.T. kp wie #absichtlich kein Kommentar, damit das beim probieren sofort gefunden wird!
+        for player in self.players:
+            if player.hitpoints > 0:
+                self.getPlayerMovement(player)
+                #An die Clients weitergeben
+                #Waffen direction muss auch geholt werden, fuer die Grafik & an Clients geschickt werden
     
     def updateAllShots(self):
         for bullet in self.aktiveShots:
@@ -72,27 +85,19 @@ class Game():
     def __handleCollision(self, bullet):
         #hole die Positionen der Shots & prueft fuer jede Kugel ob sie in einer Wand, oder einem Spieler ist
         #falls ja, nimm den shot raus beim Spiler wird hp abgezogen & ueberpruefung ob er Tod ist
-        bulletPos = bullet.getPosition()
-        if bulletPos in self.wallCoordinates:# <-- wallcoords muessen erst iwie geholt werden
+
+        bulletRect = bullet.getRect() # <-- Position mit umfang!
+        if bulletRect in self.wallCoordinates:# <-- wallcoords/Objekte werden gebraucht & abgleich mit denen, kp pb das so funktioniert
             self.activeShots.remove(bullet)
         for i in range(len(self.players)):
-            if bulletPos == players[i].getPosition():# <-- playerumfang fehlt noch sonst muesste man einen
-                self.playerIsHit(players[i], bullet) # bestimmten Pixel treffen
+            if bulletRect == players[i].getRect():#abbleich mit den Rectzeugs (pygame.rect dokumentation)
+                self.playerIsHit(players[i], bullet) 
             self.playerIsHit()
             self.activeShots.remove(bullet)
     
     def playerIsHit(self, player, bullet):
         newHp = player.getHitpoints() - bullet.getDamge()
         player.setHitpoints(newHp)
-        if newHp <= 0:
-            self.players.remove(player)
-
-    def alivePlayers(self):
-        alivePlayers = []
-        for alivePlayer in self.players:
-            if aliveplayer.getHitpoints > 0:
-                alivePlayers.append(alivePlayer)
-        return alivePlayers
     
     def addPlayer(self, player):
         self.players.append(player)
