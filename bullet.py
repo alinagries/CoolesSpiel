a31576ecc17f7d6cba1641d4a7b8f13b66579dee
LucasVoelz <Lucas.voelz@googlemail.com>
2016-12-16 15:26:33 +0100

    Kommentare

--- bullet.py
+++ bullet.py
@@ -1,33 +1,38 @@
 # -*- coding: cp1252 -*-
-#Datum :    07-14.12.16
+#Datum :    07.12.16
 #Autor/en:  Lucas V.
 #Version:   1.0
 
+from sectorialdivision import SectorialDivision
 from entity import Entity
 import math
 
-'''
-Ein Bullet ist eine Kugel mit einer Position, Bewegungsrichtung, Geschwindigkeit und Schaden.
-Ein Bullet wird immer von einer Waffe erzeugt
-'''
-
-'''
-Todo:
- -eine an und abmeldeautomatik fuer Sektoren (in arbeit)
-'''
- 
 class Bullet(Entity):
-    def __init__(self, rect = (0, 0, 1, 2), direction = (1, 1), speed = 1, damage = 2):
-        Entity.__init__(self, rect, direction, speed)
+    def __init__(self, rect = (0, 0, 1, 2), speed = 1, damage = 2):
+        '''
+        Initialisation von Bullet
+        Parameter:      rect
+                            int mit der distanz nach links
+                            int mit der disanz nach oben
+                            int width von dem Objekt
+                            int heigth von dem Objekt
+                        Flaot speed von der Bullet (Bewegungsgeschwindigkeit)
+                        Flaot damage von der Bullet
+        return values:  -
+        '''
+        Entity.__init__(self, rect, speed)
         self.dmg = damage
         self.__pixelLength = 10 #Int
         self.__r = self.__pixelLength / math.hypot(direction[0], direction[1]) #Int / (Int^2 + Int^2) 
         #r * |direction| = pixelLength --> r = pixelLength / |direction| --> r * |direction| kann alle 10 pixel (self.__pixelLength) berechnet werden
         ##|direction| = directionlaenge in Pixel        
         
-        
     def move(self):
-        #setzt die Kugel __r * speed pixel in Vektorrichtung weiter
+        '''
+        berechnet & setzt die naechste position der Bullet
+        Parameter:      -
+        return values:  -
+        '''
         x = self.rect[0] #Int
         y = self.rect[1] #Int
         directionX = self.direction[0] #Int
@@ -38,8 +43,20 @@ class Bullet(Entity):
         self.rect[0] = int(newXPosition)
         self.rect[1] = int(newYPosition)
         
-    def getDamage(self):#Int
+##################### Getters und Setters, die momentan nirgends gebraucht werden, aber dazu gehoeren #####################
+        
+    def getDamage(self):
+        '''
+        gibt dem den Schaden der Bullet aus
+        Parameter:      -
+        return values:  Float damage der Bullet
+        '''
         return self.dmg
     
-#    def setDamage(self, dmg): #Int
-#        self.dmg = dmg
+    def setDamage(self, dmg):
+        '''
+        setzt den Schaden des Objektes Bullet
+        Parameter:      Float damage
+        return values:  -
+        '''
+        self.dmg = dmg
