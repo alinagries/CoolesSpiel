0d0b5e831e3e8b796a034aca5ce047ae2b995ebb
LucasVoelz <Lucas.voelz@googlemail.com>
2016-12-28 18:07:40 +0100

    Waffe, shoot,hp, getters/setters eingebaut
    Der Spieler kann jetzt eine Waffe haben, hat eine eigene shoot-Funktion, kann 'sterben' und getters und setters wurden eingebracht

--- Niclas-Gregor/split/player.py
+++ Niclas-Gregor/split/player.py
@@ -2,44 +2,147 @@
 #Datum:     ????
 #Version:   1.0
 
+from screen import Screen
+from weapon import Weapon
 import pygame
 
-RED = (255, 0, 0)
-screen_width = 700
-screen_height = 400
-screen = pygame.display.set_mode([screen_width, screen_height])
+GREEN = (0, 255, 0)
+screensize = Screen().screensize
+screenWidth = screensize[0]
+screenHeight = screensize[1]
+screen = pygame.display.set_mode([screenWidth, screenHeight])
 
 class Player(pygame.sprite.Sprite):
     """ This class represents the Player. """
  
-    def __init__(self):
+    def __init__(self, nick = 'Lucas', hitPoints = 20, speed = 5):
         """ Set up the player on creation. """
         # Call the parent class (Sprite) constructor
         super(Player,self).__init__()
  
         self.image = pygame.Surface([20, 20])
-        self.image.fill(RED)
- 
         self.rect = self.image.get_rect()
+        self.image.fill(GREEN)
+        
+        self.nick = nick
+        self.hp = hitPoints
+        self.speed = speed
+        self.secondaryWeapon()
  
     def update(self):
         '''
         updated Den Playerbewegungen beim (knopfdruck events)
-        -wird evtl. ueberschrieben-
+         -wird evtl. ueberschrieben-
         Parameter:      -
         return Value:   -
         '''
-        screen_width = 700
-        screen_height = 400
-        speed = 5
-        """ Update the player's position. """
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
-            self.rect.move_ip(0, -speed)
+            self.rect.move_ip(0, -self.speed)
         if keys[pygame.K_s]:
-            self.rect.move_ip(0, speed)
+            self.rect.move_ip(0, self.speed)
         if keys[pygame.K_a]:
-            self.rect.move_ip(-speed, 0)
+            self.rect.move_ip(-self.speed, 0)
         if keys[pygame.K_d]:
-            self.rect.move_ip(speed, 0)
+            self.rect.move_ip(self.speed, 0)
         self.rect.clamp_ip(screen.get_rect())
+
+    def shoot(self, xPos, yPos, eventPos):
+        '''
+        Wenn moeglich (hoechstens wegen der zu vielen schussversuchen pro
+        Sekunde nicht moeglich) wird ein Objekt Bullet erstellt.
+        Falls keine Munition mehr vorhanden ist auf Standartwaffe wechseln
+        Parameter:      int xPos, die x-Position des Spielers
+                        int yPos, die y-Position des Spielers
+                        Tuple (int, int), das Ziel des Schusses
+        return values:  Boolean ODER None
+                            je nachdem ob die Waffe einen schuss abfeuern darf
+                            bsp. bei einer Waffengeschwindigkeit von 1 darf nach
+                            0.5 Sekunden kein weiterer Schuss abgefeuert werden
+        '''
+        bullet = None
+        position = xPos, yPos
+        weapon = self.weapon
+        try:
+            bullet = weapon.createBullet(position, eventPos, self.nick)#eventPos = Zielposition
+        except:
+            print 'bullet wird nicht erzeugt'
+        if weapon.ammo == 0:
+            self.secondaryWeapon()
+            
+        return bullet
+        
+    def secondaryWeapon(self):
+        '''
+        Der Spieler bekommt das Objekt der Standartwaffe zugewiesen
+        Parameter:      -
+        return values:  -
+        '''
+        self.weapon = Weapon()
+        
+    def isHit(self, damage):
+        '''
+        errechnung der Spielerhitpoints nach einem treffer
+        Parameter:      Float, damage gegen den Spieler
+        return values:  -
+        '''
+        #resistance = 0
+        #for armor in self.armors:
+        #    resistance += armor.getResistance()
+        #    armor.use()
+        #    if armor.durability == 0:
+        #        self.removeArmor(armor)
+        #remainingDamage = damage - resistance
+        #if remainingDamage > 0:
+        #    self.hp -= remainingDamage
+        self.hp -= damage    
+            
+##################### Getters und Setters, die momentan nirgends gebraucht werden, aber dazu gehoeren #####################
+
+    def getNick(self):
+        '''
+        Ausgabe des Spielernicknames
+        Parameter:      -
+        return values:  String, nickname des Spielers
+        '''
+        return self.nick
+    
+    def setNick(self, nick):
+        '''
+        setzt den Spielernickname
+        Parameter:      String, nickname des Spielers
+        return values:  -
+        '''
+        self.nick = nick
+        
+    def getHitpoints(self):
+        '''
+        Ausgabe der Spielerhitpoints
+        Parameter:      -
+        return values:  Float hitpoints des Spielers
+        '''
+        return self.hp
+    
+    def setHitpoints(self, hp):
+        '''
+        setzt die Spielerhitpoints
+        Parameter:      Float, Spielerhitpoints
+        return values:  -
+        '''
+        self.hp = hp
+    
+    def getWeapon(self):
+        '''
+        Ausgabe der Waffe des Spielers
+        Parameter:      -
+        return values:  Objekt, von einer der Klasse Weapon
+        '''
+        return self.weapon
+    
+    def setWeapon(self, weapon):
+        '''
+        Der Spieler bekommt ein Objekt der Klasse Weapon, auf self.weapon
+        Parameter:      Objekt der Klasse Weapon
+        return values:  -
+        '''
+        self.weapon = weapon
