211b3b81ba23caa50280b4344536df2a02308cb8
AlinaGri <alina.gries@t-online.de>
2017-02-06 09:31:57 +0100

    Add files via upload

--- Server Neu/gameLogic.py
+++ Server Neu/gameLogic.py
@@ -1,4 +1,5 @@
 from threading import RLock
+import threading
 from game import Game
 
 
@@ -7,71 +8,44 @@ b=RLock()
 b.acquire()
 
 
-class GameLogic(self, players):
-    def __init__(self):
-        self.game = Game()
+class GameLogic:
+    def __init__(self, players):
+        #self.game = Game()
         self.players = players
-        #self.setPlayers()
         self.playerPositions = []
 
-
     def onTick(self):
-        #line = ""
-        positions = [line + str(pos) for pos in self.playerPositions]
+        while 1:
+            line = "p"
+            for pos in self.playerPositions:
+                line = line + "_" + str(pos)
+            for player in self.players:
+                player.send(positions)
+
+    def shoot(self, clickedPos, aktPos):
+        shot = str(clickedPos) + "_" + str(aktPos)
         for player in self.players:
-            player.send(positions)
+            player.send("s_" + shot)
 
-
-
-    def setPlayers(self):
-        self.game.setPlayers(self.players)
-
+    def updatePlayerPosition(self, nick, newPosition):
+        index = self.players.index(nick)
+        self.playerPositions[index] = newPosition
         
-    def updatePlayerPosition(self,data):
-        '''
-        Wird von gameReqestHandler aufgerufen.
-        Updated die Positionen eines Spielers aus der GUI.
-        Parameter: data (Eine Liste aus Tupeln, die aus Nickname und Position des Spielers zusmgesetzt sind)
-        Rückgabewerte: -
-        '''
-        nick = data[0]
-        pos = data[1]
-        for player in self.game.players:
-            if player.nick == name:
-                player.setPosition(pos)
-                self.playerPositions[self.players.index(player)] = pos
-                
-
-    def shoot(self, direction, nick):
-        '''
-        Wird vom gameRequesthandler aufgerufen.
-        Parameter: direction (der Kugel)
-                   nick (name des Players)
-        Rückgabewerte : -
-        '''
-        for player in self.game.players:
-            if player.nick == nick:
-                bullet = player.shoot(player.position, direction)
-        self.game.addShot(bullet)
-        self.game.shoot()
+##      regeln wir in der Lobby
+##    def getAllPlayerNames(self):
+##        line = ""
+##        for player in self.game.players:
+##            line = line + player.name + " "
+##        return line
 
 
-    def getAllPlayerNames(self):
-        line = ""
-        for player in self.game.players:
-            line = line + player.name + " "
-        return line
+class GameListener:
+    def __init__(self):
 
 
-    def getPlayersWeapon(self, nick):
-        for player in self.game.players:
-            if player.nick == nick:
-                return player.weapon.getName()
 
-
-    def updatePlayer(self, nick):
-        for player in self.game.players:
-            if player.nick == nick:
-                return player.getPosition() + "," + player.getHitPoints()
+    def listen(self):
+        
+        
     
 
